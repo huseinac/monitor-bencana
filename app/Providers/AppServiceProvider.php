0038701a2d6c30ee706f85c7d5199b460e50fdbc
Husein Aditya <huseinac@gmail.com>
2026-05-04 23:27:02 +0700

    railway config 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         require_once app_path('Helpers/IoHelper.php');
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
+            URL::forceScheme('https');
+        }
     }
 }
